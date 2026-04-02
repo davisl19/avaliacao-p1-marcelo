1a0f931e4a6d0a325c50e19977fbaaa42d1a3739
gabrielcorsini-source <gabriel.corsini@sou.unifeob.edu.br>
2026-04-02 19:49:16 -0300

    Delete app_routes.dart

--- app_routes.dart
+++
@@ -1,6 +0,0 @@
-abstract class AppRoutes {
-  static const String splash = '/splash';
-  static const String login = '/login';
-  static const String register = '/register';
-  static const String home = '/home';
-}
